1ea554fa27673b9c14c894d28b9aac3c26a85e3b
Mikalai Akhimenka <mikalaialkhimenka@gmail.com>
2022-10-02 16:29:26 +0200

    Solved

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int a = 99;
+        int b = 104;
+        int c = 97;
+        int d  = 114;
+        char a1 = (char) a;
+        char b1 = (char) b;
+        char c1 = (char) c;
+        char d1 = (char) d;
+
+        System.out.println(a1);
+        System.out.println(b1);
+        System.out.println(c1);
+        System.out.println(d1);
     }
 }
